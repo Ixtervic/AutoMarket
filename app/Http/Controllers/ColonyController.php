1736b39b96e5cbdd7b35a6e129441e0cfc1beeb9
Hazel Cruz <jhhc05@micorreo.upp.edu.mx>
2025-03-05 01:38:03 -0600

    primer comit del proyecto + -mcrf creados

---
+++ app/Http/Controllers/ColonyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Colony;
+use Illuminate\Http\Request;
+
+class ColonyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Colony $colony)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Colony $colony)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Colony $colony)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Colony $colony)
+    {
+        //
+    }
+}
